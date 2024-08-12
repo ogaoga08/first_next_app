d3e92a39dd50ac199fe5b1efff20b0bc0e045350
ogaoga08 <kamekameshin0514@gmail.com>
2024-08-13 00:31:42 +0900

    画像ファイルup・Imageコンポーネントimport

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,5 +1,16 @@
-const Test = () => {
-  return <p>Homeページ</p>;
-};
- 
-export default Test;
+import Image from "next/image";
+
+const Home = () => {
+  return (
+		<div>
+			<p>Homeページ</p>
+			<Image
+            src="/cattle_y.png" //配置した画像のパスを記述する。
+            alt="Top Image"
+            layout="fill"
+            objectFit="cover"
+          />
+		</div>
+	)
+}
+export default Home;
